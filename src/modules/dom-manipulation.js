18a4207a6c749ccd9269909df7e2c866239941e6
jfoyarzo <foyarzo621@gmail.com>
2022-10-18 15:52:19 -0300

    Add displayAnimals function on dom-manipulation.js for displaying the animals in the grid

--- src/modules/dom-manipulation.js
+++ src/modules/dom-manipulation.js
@@ -1,8 +1,28 @@
-import getAnimals from "./api-requests.js";
+import getAnimals from './api-requests.js';
 
 const displayAnimals = async () => {
   const animals = await getAnimals(10);
   const fragment = new DocumentFragment();
+  const container = document.querySelector('.cards-container');
+  const parser = new DOMParser();
+  animals.forEach((element, index) => {
+    const cardStr = `<div class="card col-12 col-md-4">
+    <img src="${element.image_link}" class="card-img-top" alt="${element.name} image">
+    <div class="card-body">
+      <h5 class="card-title">${element.name}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">${element.latin_name}</h6>
+      <ul class="card-text">
+      <li>Habitat: ${element.habitat}</li>
+      <li>Diet: ${element.diet}</li>
+      <li>Location: ${element.geo_range}</li>
+      </ul>
+      <a href="#" class="btn btn-primary" data-index="${index}">Comments</a>
+    </div>
+  </div>`;
+    const card = parser.parseFromString(cardStr, 'text/html').body.firstChild;
+    fragment.appendChild(card);
+  });
+  container.appendChild(fragment);
 };
 
 export default displayAnimals;
