f1cb110449edd26829610b1174795e71c4faf67f
jfoyarzo <foyarzo621@gmail.com>
2022-10-18 16:47:47 -0300

    Finish basic styling and layout for animal cards

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 import './style.css';
 
 import displayAnimals from './modules/dom-manipulation.js';
--- src/modules/dom-manipulation.js
+++ src/modules/dom-manipulation.js
@@ -6,9 +6,10 @@ const displayAnimals = async () => {
   const container = document.querySelector('.cards-container');
   const parser = new DOMParser();
   animals.forEach((element, index) => {
-    const cardStr = `<div class="card col-12 col-md-4">
+    const cardStr = `<div class="col-12 col-md-4">
+    <div class="card mb-3">
     <img src="${element.image_link}" class="card-img-top" alt="${element.name} image">
-    <div class="card-body">
+    <div class="card-body d-flex flex-column">
       <h5 class="card-title">${element.name}</h5>
       <h6 class="card-subtitle mb-2 text-muted">${element.latin_name}</h6>
       <ul class="card-text">
@@ -16,7 +17,8 @@ const displayAnimals = async () => {
       <li>Diet: ${element.diet}</li>
       <li>Location: ${element.geo_range}</li>
       </ul>
-      <a href="#" class="btn btn-primary" data-index="${index}">Comments</a>
+      <a href="#" class="btn btn-primary align-self-end mt-auto" data-index="${index}">Comments</a>
+    </div>
     </div>
   </div>`;
     const card = parser.parseFromString(cardStr, 'text/html').body.firstChild;
