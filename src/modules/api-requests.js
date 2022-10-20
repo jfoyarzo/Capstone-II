67415f121377a6bee6721fe19600d1a028c49489
jfoyarzo <foyarzo621@gmail.com>
2022-10-19 23:41:13 -0300

    Add like counter to homepage

--- src/modules/api-requests.js
+++ src/modules/api-requests.js
@@ -1,8 +1,14 @@
-const url = 'https://api.tvmaze.com/search/shows?q=';
+const showsUrl = 'https://api.tvmaze.com/search/shows?q=';
+const likesUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/DxgcbWZ6MHoTsGdusWNs/likes/';
 
 const getShows = async (letter) => {
-  const result = await fetch(`${url}${letter}`).then((response) => response.json());
+  const result = await fetch(`${showsUrl}${letter}`).then((response) => response.json());
   return result;
 };
 
-export default getShows;
+const getLikes = async () => {
+  const result = await fetch(`${likesUrl}`).then((response) => response.json());
+  return result;
+};
+
+export { getShows, getLikes };
--- src/modules/dom-manipulation.js
+++ src/modules/dom-manipulation.js
@@ -1,12 +1,18 @@
-import getShows from './api-requests.js';
+import { getShows, getLikes } from './api-requests.js';
 
 const displayShows = async () => {
   const data = await getShows('c');
+  const likes = await getLikes();
   const fragment = new DocumentFragment();
   const container = document.querySelector('.cards-container');
   const parser = new DOMParser();
   data.forEach((element, index) => {
     const { show } = element;
+    const item = likes.find((e) => e.item_id === show.id);
+    let likeCount = 0;
+    if (item) {
+      likeCount = item.likes;
+    }
     const cardStr = `<div class="col-12 col-md-4">
     <div class="card mb-3">
     <img src="${show.image.original}" class="card-img-top" alt="${show.name} image">
@@ -14,9 +20,9 @@ const displayShows = async () => {
       <div class="d-flex justify-content-between">
           <h5 class="card-title">${show.name}</h5>
           <div class="btn-wrapper d-flex">     
-        <div class="like-wrapper text-center me-3" data-index="${index}" role="button">          
+        <div class="like-wrapper text-center me-3" data-id="${show.id}" role="button">          
           <i class="fa-regular fa-heart fa-xl like"></i>
-          <p>2</p>
+          <p>${likeCount}</p>
         </div>      
         <div class="comments-wrapper text-center" data-index="${index}" role="button">          
         <i class="fa-regular fa-comment-dots fa-xl"></i>
