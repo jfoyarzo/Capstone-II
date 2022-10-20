3389ab668a4a6ff4feaea934977f9dbfd18f175f
Miliyonayalew <miliayalew@gmail.com>
2022-10-20 23:09:59 +0300

    Add display comment and comment form to the modal

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -29,7 +29,18 @@ const modalMethods = {
             <h5 class="text-start ps-4 text-muted">Runtime: ${result.runtime}</h5>
             <h5 class="text-start ps-4 text-muted">Rating: ${result.rating.average}</h5>
             <p  class="text-start ps-4 text-muted ps-4>${result.genres}</p>
-            
+            <form id="form" method="post" class='modalForm mt-2'>
+            <h3></h3>
+            <p>Comented <span id="commentCount" class="badge rounded-pill bg-info text-dark"></span> times</p>
+            <div class='justify-content-center input-group-sm'>
+              <input class="form-control formInput" type="text" placeholder="Your name" name="name" id="nameMealID" required>
+              <textarea class="form-control formInput mt-2" type="text" placeholder="Your comment" name="comment" id="commentMealID" required></textarea>
+            </div>
+            <div class="modal-footer mt-2 ma-0">
+              <input type="submit" value="Comment" class="submit btn btn-primary float-end" id="submit">
+            </div>
+          </form>
+          <div class="commentSection"></div>
           </div>
         </div>
 `;
