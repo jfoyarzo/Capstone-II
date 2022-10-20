b3d9ee102148d7b8c429a95bec7bdbcacc01c936
Miliyonayalew <miliayalew@gmail.com>
2022-10-20 23:37:07 +0300

    Add comment api request and comment form

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,9 +1,12 @@
+import commentApi from './commenAPI.js';
+
 const modalMethods = {
   async movie(movieName) {
     const modal = document.querySelector('#staticBackdrop');
     modal.innerHTML = '';
     const url = `https://api.tvmaze.com/singlesearch/shows?q=${movieName}`;
     const result = await fetch(url).then((response) => response.json());
+    console.log(result.id);
     if (result.rating.average === null) {
       result.rating.average = 'No rating';
     }
@@ -33,8 +36,8 @@ const modalMethods = {
             <h3></h3>
             <p>Comented <span id="commentCount" class="badge rounded-pill bg-info text-dark"></span> times</p>
             <div class='justify-content-center input-group-sm'>
-              <input class="form-control formInput" type="text" placeholder="Your name" name="name" id="nameMealID" required>
-              <textarea class="form-control formInput mt-2" type="text" placeholder="Your comment" name="comment" id="commentMealID" required></textarea>
+              <input class="form-control formInput" type="text" placeholder="Your name" name="name" id="showID" required>
+              <textarea class="form-control formInput mt-2" type="text" placeholder="Your comment" name="comment" id="commentshowID" required></textarea>
             </div>
             <div class="modal-footer mt-2 ma-0">
               <input type="submit" value="Comment" class="submit btn btn-primary float-end" id="submit">
@@ -45,6 +48,30 @@ const modalMethods = {
         </div>
 `;
     modal.appendChild(allContent);
+
+    // Event listener for the form
+    const form = document.querySelector('form');
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const username = document.querySelector('#showID').value;
+      const comment = document.querySelector('#commentshowID').value;
+      commentApi.addComment(result.id, username, comment);
+
+      const date = new Date();
+      const creationDate = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
+
+      const commentHtml = document.createElement('p');
+      commentHtml.innerHTML = `<span class="badge rounded-pill">${username} ${creationDate}:</span> ${comment}`;
+
+      const commentList = document.querySelector('.commentSection');
+      commentList.appendChild(commentHtml);
+
+      const commentCount = document.querySelector('#commentCount');
+      const nextCount = Number(commentCount.textContent) + 1;
+      commentCount.textContent = nextCount;
+
+      form.reset();
+    });
   },
 };
 
