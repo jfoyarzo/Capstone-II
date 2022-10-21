41c64f6ba00bb95c1a7724eff1df49b5ce7a8e24
Miliyonayalew <miliayalew@gmail.com>
2022-10-21 04:15:16 +0300

    Fix merge conflict issue

--- src/index.js
+++ src/index.js
@@ -2,12 +2,21 @@ import './style.css';
 import displayShows from './modules/dom-manipulation.js';
 import modal from './modules/modal.js';
 
-displayShows();
-const card = document.querySelector('.cards-container');
-card.addEventListener('click', (e) => {
-  const text = e.target.textContent;
-  if (text.trim() === 'Details') {
-    modal.movie(e.target.id);
-    modal.getComments(e.target.id);
-  }
-});
+displayShows()
+  .then(() => {
+    const likeBtns = document.querySelectorAll('.like-wrapper');
+    likeBtns.forEach((element) => {
+      const { id } = element.dataset;
+      element.addEventListener('click', () => {
+        updateLikes(id, element);
+      }, { once: true });
+    });
+    const card = document.querySelector('.cards-container');
+    card.addEventListener('click', (e) => {
+      const text = e.target.textContent;
+      if (text.trim() === 'Details') {
+        modal.movie(e.target.id);
+        modal.getComments(e.target.id);
+      }
+    });
+  });
