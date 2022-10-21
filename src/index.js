e7b97ef2a90504f691085149a0e73529dd2eea57
Miliyonayalew <miliayalew@gmail.com>
2022-10-21 03:59:40 +0300

    Add show comment functionality

--- src/index.js
+++ src/index.js
@@ -8,5 +8,6 @@ card.addEventListener('click', (e) => {
   const text = e.target.textContent;
   if (text.trim() === 'Details') {
     modal.movie(e.target.id);
+    modal.getComments(e.target.id);
   }
 });
